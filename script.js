246650b6ba180d7f9c1632bbe342545c434865e0
KAIQUE PEREIRA DA CONCEICAO <kaique.pconceicao1@senacsp.edu.br>
2024-09-25 15:11:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Solicitar nota do aluno
+let nota = parseInt (prompt ("Insira sua nota"));
+
+
+// Verificar se a nota é maior ou menor que 7
+if (nota >= 7){
+    alert("Aluno aprovado");
+} 
+else {
+    alert("Aluno reprovado");
+}
